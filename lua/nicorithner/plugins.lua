5b85535ecc8640d43c95235e5ab3a1814cb3617d
Nico Rithner <nicorithner@gmail.com>
2021-12-24 20:20:00 -0700

    Add nvim-ts-rainbow

--- lua/nicorithner/plugins.lua
+++ lua/nicorithner/plugins.lua
@@ -73,6 +73,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+  use "p00f/nvim-ts-rainbow"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
