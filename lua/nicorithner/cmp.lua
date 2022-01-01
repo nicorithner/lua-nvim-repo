414c4c2262160109208ae93d6961f9c0f4beaceb
Nico Rithner <nicorithner@gmail.com>
2022-01-01 16:13:48 -0700

    Fix type in cmp.lua

--- lua/nicorithner/cmp.lua
+++ lua/nicorithner/cmp.lua
@@ -101,8 +101,8 @@ cmp.setup {
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
-        luasnip = "[LSP]",
-        luasnip = "[NVIM_LUA]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[NVIM_LUA]",
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
