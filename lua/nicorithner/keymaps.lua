6ed1fa08bd69d7650ff5e874ca58ddb8b7fa2e01
Nico Rithner <nico@altvia.com>
2022-01-04 16:27:07 -0700

    Add new keymaps

--- lua/nicorithner/keymaps.lua
+++ lua/nicorithner/keymaps.lua
@@ -20,13 +20,28 @@ vim.g.maplocalleader = " "
 -- General --
 
 keymap("n", "<leader>k", ":nohlsearch<CR>", opts)
+
+-- LSP
 keymap("n", "<leader>F", ":lua vim.lsp.buf.formatting()<CR>", opts)
+keymap("n", "<silent>gr", ":lua vim.lsp.buf.references()<CR>", opts)
+keymap("n", "<silent>gd", ":lua vim.lsp.buf.definition()<CR>", opts)
+keymap("n", "<silent>gR", ":lua vim.lsp.buf.rename()<CR>", opts)
 
 -- Telescope
 keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
--- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
 keymap("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", opts)
+keymap("n", "<leader><space>", "<cmd>Telescope git_files<CR>", opts)
+keymap("n","<leader>fd", ":lua telescope_find_files_in_path()<CR>", opts)
 
 -- Nvimtree
 keymap("n", "<leader>n", ":NvimTreeToggle<cr>", opts)
+
+-- " tpope/vim-fugitive
+keymap("n", "<leader>gg", "<cmd>G<cr>", opts)
+keymap("n", "<leader>gdiff", "<cmd>G diff<cr>", opts)
+
+-- "--- szw/vim-maximizer
+-- nnoremap <silent> <C-w>m :MaximizerToggle!<CR>
+keymap("n", "<silent><C-w>m", ":MaximizerToggle!<CR>", opts)
+
