6f7311f397da05407fd47c166e9dcfa50ab86cd1
Nico Rithner <nico@altvia.com>
2022-04-18 08:54:05 -0600

    Update cmp

--- lua/nicorithner/cmp.lua
+++ lua/nicorithner/cmp.lua
@@ -51,6 +51,10 @@ cmp.setup {
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
 		["<C-j>"] = cmp.mapping.select_next_item(),
--- lua/nicorithner/treesitter.lua
+++ lua/nicorithner/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
@@ -21,7 +21,7 @@ configs.setup {
     enable_autocmd = true,
   },
   rainbow = {
-    enable = false,
+    enable = true,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
